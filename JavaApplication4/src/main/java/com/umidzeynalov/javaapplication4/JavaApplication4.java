d7be0160d4e5d109e7bddcbfaf25ff0743dc4a32
Umid Zeynalov <zeynalovumid0198@gmail.com>
2024-08-27 07:04:52 -0800

    menim ilk repositorim

---
+++ JavaApplication4/src/main/java/com/umidzeynalov/javaapplication4/JavaApplication4.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.umidzeynalov.javaapplication4;
+
+/**
+ *
+ * @author Asus
+ */
+public class JavaApplication4 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
